eb13f33fec4ebcbeff50bd1e096887c9baef8df4
Berkay <56292568+Berkayyolcu@users.noreply.github.com>
2022-11-14 22:26:46 +0300

    Add files via upload

---
+++ Html + Js - change the font color/font_color.js
@@ -0,0 +1,18 @@
+function red(){
+document.getElementById("p").style.color="red";
+}
+
+function green(){
+document.getElementById("p").style.color="green";
+}
+
+function orange() {
+document.getElementById("p").style.color="orange";
+}
+
+function black() {
+document.getElementById("p").style.color="black";
+}
+
+
+
